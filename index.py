bf135f66bb11bbfc61c3eb6830fa6534c292deff
Edward Bruce <40115101@live.napier.ac.uk>
2016-10-20 23:01:04 +0000

    Added to current code

--- index.py
+++ index.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, abort
 app = Flask(__name__)
 
 @app.route('/')
 def root():
   return render_template('base.html'), 200
 
-if __name__ == "__main__":
+#@app.route('/Playstation-4')
+#def root_Playstation-4():
+   # return render_template('Playstation-4.html')
+
+@app.route('/img')
+def img():
+  start = '<img src="'
+  url = url_for('static', filename='uncharted4.jpg')
+  end = '">'
+  return start+url+end, 200
+
+if __name__ == ("__main__"):
   app.run(host='0.0.0.0', debug=True)
 
