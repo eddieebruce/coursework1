5c2ca240701ec846f08ad5ce6beea77068122c48
Edward Bruce <40115101@live.napier.ac.uk>
2016-10-21 22:01:56 +0000

    Added Images

--- index.py
+++ index.py
@@ -5,17 +5,6 @@ app = Flask(__name__)
 def root():
   return render_template('base.html'), 200
 
-#@app.route('/Playstation-4')
-#def root_Playstation-4():
-   # return render_template('Playstation-4.html')
-
-@app.route('/img')
-def img():
-  start = '<img src="'
-  url = url_for('static', filename='uncharted4.jpg')
-  end = '">'
-  return start+url+end, 200
-
-if __name__ == ("__main__"):
+if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
 
