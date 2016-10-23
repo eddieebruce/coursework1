86670799a7ceb2860e9899a095e3cd8d2da44974
Edward Bruce <40115101@live.napier.ac.uk>
2016-10-23 17:42:21 +0000

    Imroved the style of layout, finished xbox section

--- index.py
+++ index.py
@@ -9,6 +9,14 @@ def root():
 def root_Playstation4():
   return render_template('Playstation4.html'), 200
 
+@app.route('/XboxOne/')
+def root_XboxOne():
+  return render_template('Xboxone.html'), 200
+
+@app.route('/WiiU/')
+def root_WiiU():
+  return render_template('Wiiu.html'), 200
+
 if __name__ == ("__main__"):
   app.run(host='0.0.0.0', debug=True)
 
